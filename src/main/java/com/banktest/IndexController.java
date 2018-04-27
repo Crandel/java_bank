4b7f71a9c498b7acacf0e7554e39d1c77f6f9cb5
Crandel <cradlemann@gmail.com>
2018-04-27 16:08:36 +0300

    Add Spring Security for test

--- src/main/java/com/banktest/IndexController.java
+++ src/main/java/com/banktest/IndexController.java
@@ -7,11 +7,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class IndexController {
     @GetMapping("/test")
-    public IndexModel index(@RequestParam(value="id", defaultValue="10") String str_id) {
+    public IndexModel test(@RequestParam(value="id", defaultValue="10") String str_id) {
         try {
 
             long id = Long.parseLong(str_id);
             return new IndexModel(id);
+        } catch (NumberFormatException e){
+            return new IndexModel(10);
+        }
+    }
+
+    @GetMapping("/index")
+    public IndexModel index(@RequestParam(value="id", defaultValue="10") String str_id) {
+        try {
+
+            long id = Long.parseLong(str_id);
+            return new IndexModel(id + 100);
         } catch (NumberFormatException e){
             return new IndexModel(10);
         }
